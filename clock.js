c00ac6c90a8ccc1a1f3ea2de9e7460c9362eac55
KarthikeyaDevarakonda <karthikeya8787@gmail.com>
2024-12-03 20:55:09 +0530

    deploy

--- clock.js
+++ clock.js
@@ -1,12 +1,14 @@
 const seconds = document.getElementById('seconds')
 const minutes = document.getElementById('minutes')
 const hours = document.getElementById('hours')
+const ampm = document.getElementById('am-pm')
+
 
 const startBtn = document.getElementById('start-btn')
 const stopBtn = document.getElementById('stop-btn')
 const resetBtn = document.getElementById('reset-btn')
 
-let i=0;
+let i=1;
 let j=0;
 let k=0;
 let timerid;
@@ -16,7 +18,7 @@ startBtn.addEventListener('click',()=>{
     startBtn.disabled = true;
     resetBtn.disabled = true;
     timerid = setInterval(()=>{
-        seconds.innerText = i < 10 ? '0' + i : i;
+        c.innerText = i < 10 ? '0' + i : i;
         i++;
         if(i === 60){
             i = 0;
@@ -32,15 +34,18 @@ let min = function(){
         j = 0;
         hour();
     }
-    minutes.innerText= j < 10 ? '0' + j : j;
+    b.innerText= j < 10 ? '0' + j : j;
 }
 
 let hour = function(){ 
     k++;
     if(k === 12){
-        k = 0;
+        ampm.innerText = ampm.innerText === "AM" ? "PM" : "AM";
     }
-    hours.innerText= k < 10 ? '0' + k : k;
+    else if(k === 13){
+        k=1;
+    }
+    a.innerText= k < 10 ? '0' + k : k;
 }
 
 stopBtn.addEventListener('click',()=>{
@@ -49,10 +54,11 @@ stopBtn.addEventListener('click',()=>{
     resetBtn.disabled = false;
 })
 resetBtn.addEventListener('click',()=>{
-      seconds.innerText="00"
-      minutes.innerText="00"
-      hours.innerText="00"
+      a.innerText="00"
+      b.innerText="00"
+      c.innerText="00"
     i=1;
     j=0;
     k=0;
+    ampm.innerText = "AM";
 })
