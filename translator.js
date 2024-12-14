e57e174f5664552e370a82aaee04ea3a5bc9c1b0
KarthikeyaDevarakonda <karthikeya8787@gmail.com>
2024-12-14 13:09:38 +0530

    deploY

--- translator.js
+++ translator.js
@@ -7,13 +7,13 @@ btn.addEventListener('click',(e)=>{
    e.preventDefault();
    let newdata = userinput.value.trim();
    if(newdata !== ''){
-   let fetched = fetch('https://api.funtranslations.com/translate/yoda.json?text='+newdata)
+   let fetched = fetch('https://api.funtranslations.com/translate/australian.json?text='+newdata)
    .then((res)=>res.json())
    .then((data)=>{
         translated.innerText = data.contents.translated;
         userinput.value ='';
    })
-   .catch((err)=> translated.innerText ="ERROS IN FETCHING")
+   .catch((err)=> translated.innerText ="ERROR IN FETCHING")
 }else{
     alert("ENTER DATA TO CONVERT")
 }
