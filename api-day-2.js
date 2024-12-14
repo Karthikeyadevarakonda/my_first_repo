5d8aeefa35bb2224342f9ecafe10ec5cc14884ff
KarthikeyaDevarakonda <karthikeya8787@gmail.com>
2024-12-14 09:58:51 +0530

    deploy

--- api-day-2.js
+++ api-day-2.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('container');
 const btn = document.getElementById('fetch-btn')
-
+const image = document.getElementById('image')
 
 
 let breeds = fetch('https://dog.ceo/api/breeds/list/all')
@@ -12,14 +12,14 @@ let breeds = fetch('https://dog.ceo/api/breeds/list/all')
     for(let breed in breeds){
         if(breeds[breed].length === 0){
             let option = document.createElement('option');
-            option.innerHTML = breed;
+            option.innerHTML = breed[0].toUpperCase() + breed.substring(1);
             option.value = breed;
             container.appendChild(option);
         }else{
                for(let subreed of breeds[breed]){
                 let option =document.createElement('option');
-                option.innerHTML = `${subreed} ${breed}`;
-                option.value = `${subreed} ${breed}`;
+                option.innerHTML = subreed[0].toUpperCase()+subreed.substring(1) + ' ' + breed[0].toUpperCase() + breed.substring(1);;
+                option.value = `${breed} ${subreed}`;
                 container.appendChild(option);
                }
             }
@@ -28,3 +28,17 @@ let breeds = fetch('https://dog.ceo/api/breeds/list/all')
 
 })
 .catch((err)=>console.log("ERROR",err));
+
+
+btn.addEventListener('click',()=>{
+  let inputdata = container.value.trim();
+  let newData = inputdata.split(' ').join('/');
+  let addedBreedtype = newData;
+//   console.log(addedBreedtype);
+
+fetch('https://dog.ceo/api/breed/' + addedBreedtype + '/images/random')
+.then((res)=> res.json())
+.then((data)=>{
+       image.src = data.message;
+})
+})
