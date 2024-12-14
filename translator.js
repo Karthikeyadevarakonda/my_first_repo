72fa6f88895b065baf92f1087cb82e6f67f9663b
KarthikeyaDevarakonda <karthikeya8787@gmail.com>
2024-12-14 12:42:10 +0530

    deploy

---
+++ translator.js
@@ -0,0 +1,12 @@
+let x = {
+    "success": {
+      "total": 1
+    },
+    "contents": {
+      "translated": "Apple 🍏 is great fruit 🥧 fries 🍟 are yummy when you are in the car 🚡",
+      "text": "Apple is great fruit fries are yummy when you are in the car",
+      "translation": "emoji"
+    }
+  }
+
+  console.log(x.contents);
